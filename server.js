a99048e439ae3a6984f8b1925a47d15dcf76a602
jazpro-max <anyorekirejosephat@gmail.com>
2026-09-05 22:46:20 +0300

    Serve static files and add login route

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
 // ---------------------------------------------------------------------------
 // MODELS
 // ---------------------------------------------------------------------------
